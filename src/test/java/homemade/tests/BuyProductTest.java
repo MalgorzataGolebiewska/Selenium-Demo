d2896849ab8f78463d85d9a743b59ab7aaabc987
MalgorzataGolebiewska <malgorzata.golebiewska01@gmail.com>
2023-05-29 15:03:42 +0200

    adding elements to OrderDetailsPage and assertions to BuyProductTest

--- src/test/java/homemade/tests/BuyProductTest.java
+++ src/test/java/homemade/tests/BuyProductTest.java
@@ -2,6 +2,8 @@ package homemade.tests;
 
 import homemade.models.Customer;
 import homemade.pages.HomePage;
+import homemade.pages.OrderDetailsPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BuyProductTest extends BaseTest {
@@ -11,14 +13,15 @@ public class BuyProductTest extends BaseTest {
         Customer customer = new Customer();
         customer.setEmail("testerski@testy.pl");
 
-        new HomePage(driver).openShopPage().openProduct("Java Selenium WebDriver")
+        OrderDetailsPage orderDetailsPage =
+                new HomePage(driver).openShopPage().openProduct("Java Selenium WebDriver")
                 .addProductToCart()
                 .viewCartPage()
                 .openAdressDetails()
                 .fillAddressDetails(customer);
 
-
-
+        Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(),"Thank you. Your order has been received.");
+        Assert.assertEquals(orderDetailsPage.getProductName().getText(),"Java Selenium WebDriver × 1");
     }
 
 }
